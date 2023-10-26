616db83a43d0b62ab232e52a2e2ce6b865ab36ae
enrique95 <enrique_alvarado95@hotmail.com>
2023-10-25 22:14:21 -0600

    finished hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
 import Image from 'next/image';
+import GeneralWrapper from '@/components/Wrappers/General';
+import AnimatedText from '@/components/AnimatedText';
+import profileImg from '../../public/images/profile/developer-pic-1.png';
+import Link from 'next/link';
+import { LinkArrow } from '@/components/Icons';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="  font-extrabold">Hola mundo</h1>
+    <main className="flex  items-center text-dark w-full min-h-screen">
+      <GeneralWrapper className="pt-0">
+        <div className="flex items-center justify-between w-full">
+          <div className="w-1/2">
+            <Image
+              src={profileImg}
+              alt="Enrique Alvarado"
+              className="w-full h-auto"
+            />
+          </div>
+          <div className="w-1/2 flex flex-col items-center self-center">
+            <AnimatedText
+              text="Turning Vision Into Reality With Code And Design."
+              className="!text-6xl !text-left"
+            />
+            <p className="my-4 text-base font-medium">
+              As a skilled full-stack developer, I am dedicated to turning ideas
+              into innovative web applications. Explore my latest projects and
+              articles, showcasing my expertise in React.js and web development.
+            </p>
+            <div className="flex items-center self-start m-2">
+              <Link
+                className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+               border-2 border-solid border-transparent hover:border-dark  transition-all duration-150 hover:bg-light hover:text-dark"
+                href="/archivo.pdf"
+                download
+                target={'_blank'}
+              >
+                <LinkArrow /> CV / Resume
+              </Link>
+              <Link
+                href="mailto:titealvarado@gmail.com"
+                className="ml-4 text-xl font-bold capitalize"
+              >
+                Checkout my projects!
+              </Link>
+            </div>
+          </div>
+        </div>
+      </GeneralWrapper>
     </main>
   );
 }
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -66,3 +66,19 @@ export const Linkedin = () => {
     </SVGIcon>
   );
 };
+
+export const LinkArrow = () => {
+  return (
+    <SVGIcon className="w-6 mr-2" width="24" height="24" viewBox="0 0 24 24">
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M11 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-5m-7 1L20 4m-5 0h5v5"
+      />
+    </SVGIcon>
+  );
+};
